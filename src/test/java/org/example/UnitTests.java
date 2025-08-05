a67b2c81abddd94fbc8d8366c4169694411b5a5c
tolstenkaya <o.tutova.82@gmail.com>
2025-08-05 20:21:35 +0300

    Add Unit tests

--- src/test/java/org/example/UnitTests.java
+++ src/test/java/org/example/UnitTests.java
@@ -8,12 +8,37 @@ public class UnitTests {
     @Test
     void testCorrectPriceForVIP(){
         double res = calculatePrice(100, true);
-        assertEquals(90,res);
+        assertEquals(90.0,res,0.0001,"VIP price should be 10% off");
     }
 
     @Test
-    void testCorrectPriceForUsual(){
+    void testCorrectPriceForNonVIP(){
         double res = calculatePrice(100,false);
+        assertEquals(100.0,res,0.0001,"Non-VIP price should stay the same");
+    }
 
+    @Test
+    void testZeroPrice(){
+        double res = calculatePrice(0,true);
+        assertTrue(res==0);
+    }
+
+    @Test
+    void testNegativePrice(){
+        double res = calculatePrice(-100,false);
+        assertFalse(res>0,"The price should be positive");
+    }
+
+    @Test
+    void testLargePriceForVIP(){
+        double res = calculatePrice(Long.MAX_VALUE,true);
+        double expected = Long.MAX_VALUE*0.9;
+        assertEquals(expected,res,1e10);
+    }
+
+    @Test
+    void testLargePriceForNonVIP(){
+        double res = calculatePrice(Long.MAX_VALUE,false);
+        assertEquals((double)Long.MAX_VALUE,res,1e10);
     }
 }
